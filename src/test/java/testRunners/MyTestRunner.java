e493cb72fe46836041addd274b444116fb8a2f53
AnktSen <ankitsen1506@gmail.com>
2026-02-11 14:27:50 +0530

    Runner Modified

--- src/test/java/testRunners/MyTestRunner.java
+++ src/test/java/testRunners/MyTestRunner.java
@@ -4,17 +4,20 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 
-public class MyTestRunner {
-	@CucumberOptions(features = "src/test/resources/features",
-			glue = { "stepDefinition", "AppHooks"},
-			plugin= {"pretty"}
-
-	)
-	public class TestNGRunner extends AbstractTestNGCucumberTests {
-		@Override
-		@DataProvider(parallel = false)
-		public Object[][] scenarios() {
-			return super.scenarios();
-		}
-	}
+@CucumberOptions(
+    features = "src/test/resources/features",
+    glue = { "stepDefinition", "AppHooks" },
+    plugin = {
+        "pretty",
+        "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
+    },
+    monochrome = true
+)
+public class MyTestRunner extends AbstractTestNGCucumberTests {
+    
+    @Override
+    @DataProvider(parallel = false)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 }
